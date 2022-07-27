612906d72fd8acffb87dea6f94b23014cea721a4
CarterPlus <hashcat@126.com>
2022-07-28 00:59:30 +0800

    chore: 去除无谓的日志

--- hello-api/src/main/resources/thrifts/hello_domain.thrift
+++ hello-api/src/main/resources/thrifts/hello_domain.thrift
@@ -1,17 +1,6 @@
 namespace java com.dapeng.example.hello.domain
 
-/**
-* hello
-**/
 struct Hello {
-/**
-* name
-**/
     1: string name,
-
-/**
-* message
-**/
     2: optional string message
-
 }
